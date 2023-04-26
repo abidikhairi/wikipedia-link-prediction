cbecf0d6d11e5b7b60091b960d11135018adfb0b
abidikhairi <abidikhairi.1337@gmail.com>
2023-04-26 23:02:36 +0200

    download uploaded files

--- download.sh
+++ download.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+echo "Starting download of Wikipedia link prediction datasets and pre-trained word embeddings"
+
+TRAINING_EDGES=https://github.com/abidikhairi/wikipedia-link-prediction/raw/main/data/train.csv
+TEST_EDGES=https://github.com/abidikhairi/wikipedia-link-prediction/raw/main/data/test.csv
 WORD2VEC_FEATURES=1Qm54SOKmmWy066s90EjQw5hF3t9Hv8dE
 
-cd data && gdown "$WORD2VEC_FEATURES"
+if [ ! -d "data" ]; then
+  echo "Creating data directory"
+  mkdir data
+fi
+
+echo "Downloading pre-trained word embeddings"
+cd data 
+gdown "$WORD2VEC_FEATURES"
+
+echo "Downloading training and test datasets"
+wget "$TRAINING_EDGES"
+wget "$TEST_EDGES"
+
+echo "Download complete!"
